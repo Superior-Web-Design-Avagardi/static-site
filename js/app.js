83e1cd1d02bb551a13bb9f22d8177299b7a10a7a
shash7 <shashwat.amin@yahoo.com>
2014-10-20 21:17:49 +1100

    Added media queries
    Also, a couple of other minor changes.

--- js/app.js
+++ js/app.js
@@ -4,6 +4,21 @@
 (function(window, document, undefined) {
 	
 	'use strict';
+	var utils = {
+		getFormData : function(el) {
+			jQuery = jQuery || null;
+			if(el instanceof jQuery) {
+			} else {
+				el = $(el);
+			}
+			var data = $(el).serializeArray();
+			var result = {};
+			for (var key in data) {
+				result[data[key].name] = data[key].value;
+			}
+			return result;
+		}
+	};
 	
 	var fx = {
 		aboutUs : function(e) {
@@ -29,6 +44,21 @@
 	function addListners() {
 		$('[data-action="aboutUs"]').click(fx.aboutUs);
 		$('[data-action="registerInterest"]').click(fx.registerInterest);
+		
+		$('#npv').submit(function(e) {
+			e.preventDefault();
+			var data = utils.getFormData(this);
+			console.log(data);
+			var npv = data.rn/Math.pow((1+data.coc), data.year);
+			var scrap = data.scrap/Math.pow(1+data.coc, data.year);
+			console.log(npv);
+			console.log(data.rn + '/ Math.pow((' + 1 + ' + ' + data.coc + '), ' + data.year+' )');
+			console.log(scrap);
+			console.log(-data.ii);
+			var est = -data.ii + npv + scrap;
+			$(this).append('<span class="textual">' + est + '</span><br>');
+		});
+		
 		$(document).on('click', '[data-action="modalClose"]', fx.closeModal);
 	}
 	
@@ -36,8 +66,6 @@
 		var opts = {
 			mode : 'fade'
 		};
-		
-		$('.slider').bxSlider(opts);
 	}
 	
 	function initModal() {
