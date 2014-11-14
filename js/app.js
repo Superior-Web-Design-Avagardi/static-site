8c5493f8313025272054bfc8addf82f10ca599c6
shash7 <shashwat.amin@yahoo.com>
2014-11-14 19:54:13 +1100

    Changed modal library
    Added ie.css for all the ugly ie hacks

--- js/app.js
+++ js/app.js
@@ -27,16 +27,11 @@
 			}, 500);
 		},
 		registerInterest : function(e) {
-			var str = '<article class="modal-inner"><h2 class="center">Hi there</h2><span class="close icon-cross" data-action="modalClose"></span>';
-			str += '<p class="center">Please enter your email to stay up to date with our latest products</p>';
-			str += '<form><label for="email">Your email</label><br><input type="email" class="full" placeholder="name@provider.com" id="email">';
-			str += '<input type="submit" value="Subscribe" class="full"></form>';
-			str += '</article>';
-			modal.create(str);
-		},
-		closeModal : function() {
-			console.log('fawf');
-			modal.unstage();
+			var options = {
+				closeClass : 'simplemodal-close close icon-cross',
+				maxWidth : 420
+			};
+			$.modal($('#modal-template').html(), options);
 		}
 	};
 	var modal;
@@ -44,22 +39,9 @@
 	function addListners() {
 		$('[data-action="aboutUs"]').click(fx.aboutUs);
 		$('[data-action="registerInterest"]').click(fx.registerInterest);
-		
-		$('#npv').submit(function(e) {
-			e.preventDefault();
-			var data = utils.getFormData(this);
-			console.log(data);
-			var npv = data.rn/Math.pow((1+data.coc), data.year);
-			var scrap = data.scrap/Math.pow(1+data.coc, data.year);
-			console.log(npv);
-			console.log(data.rn + '/ Math.pow((' + 1 + ' + ' + data.coc + '), ' + data.year+' )');
-			console.log(scrap);
-			console.log(-data.ii);
-			var est = -data.ii + npv + scrap;
-			$(this).append('<span class="textual">' + est + '</span><br>');
+		$(document).on('click', '.close', function() {
+			$.modal.close();
 		});
-		
-		$(document).on('click', '[data-action="modalClose"]', fx.closeModal);
 	}
 	
 	function slideShow() {
@@ -69,9 +51,7 @@
 	}
 	
 	function initModal() {
-		modal = new Modal({
-			closeOnClick : false
-		});
+
 	}
 	
 	function init() {
